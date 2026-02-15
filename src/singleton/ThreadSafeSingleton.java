1df91e262b9c03ef9fa65dec43e0910a5610b4e6
suranga madhushan <150942338+surangamadhushan24@users.noreply.github.com>
2026-02-15 20:51:17 +0530

    Implement ThreadSafeSingleton

---
+++ src/singleton/ThreadSafeSingleton.java
@@ -0,0 +1,18 @@
+package singleton;
+
+
+
+public class ThreadSafeSingleton {
+
+    private static ThreadSafeSingleton instance;
+
+    private ThreadSafeSingleton(){}
+
+    public static synchronized ThreadSafeSingleton getInstance() {
+        if (instance == null) {
+            instance = new ThreadSafeSingleton();
+        }
+        return instance;
+    }
+
+}
